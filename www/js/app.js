f7afae113246e42928d4347eb7dbc2155d5829a9
arnaud tchana <arnaudtchana1994@gmail.com>
2018-08-19 00:22:40 +0100

    gestion du login avec satellizer

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,8 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-var App = angular.module('starter', ['ionic'])
+var ApiUrl = "https://at-deg.inimov-cloud.com/api/";
+var App = angular.module('starter', ['ionic','satellizer','ngStorage'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -26,7 +27,12 @@ var App = angular.module('starter', ['ionic'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider,$authProvider,$httpProvider) {
+    // Satellizer configuration that specifies which API
+    // route the JWT should be retrieved from
+    $authProvider.loginUrl = ApiUrl+'auth';
+    $authProvider.signupUrl = ApiUrl+'register';
+    $httpProvider.interceptors.push('InterceptorFactory');
   $stateProvider
 
     .state('connexion',{
@@ -121,4 +127,111 @@ var App = angular.module('starter', ['ionic'])
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/connexion');
-});
+})
+
+        .factory('InterceptorFactory',['$sessionStorage','$q','$rootScope','$localStorage','$injector', function($sessionStorage,$q,$rootScope,$localStorage,$injector,$ionicLoading){
+            return {
+                //lorsquon envoi une requette on met le token dans lentete
+                request : function(config) {
+                    //console.log("je suis ici dans la requete de sortie");
+                    config.headers.Authorization= "Bearer "+$sessionStorage.token;
+                    //console.log($sessionStorage.token);
+                    /*en envoi la requette*/
+                    return config;
+                },
+                /*dans le cas ou la requete passe avec succes on regarde si le token est present dans lentete
+                 * auquel cas on le met dans la variable de session*/
+                response : function(response){
+                    if(token = response.headers('Authorization')){
+                        /*on enregistre le token dans la varible de session*/
+                        /*on regarde sil y a le bearer dans le token de base et on le supprime*/
+                        console.log("ici on enregistre le token suivant "+token);
+                        $sessionStorage.token = token;
+                    }
+                    /*on retourne la reponse*/
+                    return response;
+                },
+                /*dans le cas ou on a une mauvaise reponse*/
+                // responseError: function(rejection){
+                //   if(rejection.status ===401 && rejection.data["error"]!="invalid_credentials"){
+                //     /*on essaie de refresh le token cote serveur*/
+                //
+                //     console.log(rejection.data["error"]);
+                //     var deffered = $q.defer();
+                //     $injector.get("$http").post(testApiUrl+"/api/refresh?token="+$sessionStorage.token,{}, {
+                //       headers: {
+                //         Authorization: "Bearer "+$sessionStorage.token
+                //       }
+                //     }).then(function(response){
+                //       /*si la requette passe, on aura un nouveau token et doit le garder dans notre storage*/
+                //       /*ici le token est dans la variable refreshtoken*/
+                //       $sessionStorage.token = response.refreshToken;
+                //       console.log("voici le token refresh" +response.refreshToken);
+                //       console.log("voici la reponse du refresh" +response);
+                //       console.log("ici on vient de refresh le token");
+                //
+                //       /*on envoie a present la requete originale*/
+                //
+                //       /*ici on enregistre le token dans config avant de renvoyer la requete*/
+                //       config.headers.Authorization= "Bearer "+$sessionStorage.token;
+                //       $injector.get("$http")(response.config)
+                //         .then(function(response){
+                //           /*si la requete originale passe, on retourne le resultat*/
+                //           return deffered.resolve(response);
+                //         },function(){
+                //           /*cette requete nest pas passee a nouveau*/
+                //           /*So we reject the response and carry on with 401*/
+                //           /*le token peut etre expire on le supprime en local et on renvoie lutilisateur en page de connexion*/
+                //           delete $sessionStorage.token;
+                //           $state.go('authentification');
+                //           return deffered.reject();
+                //         })
+                //     },function(){
+                //       /*le rafraichissement du token a echoue, on deconnecte lutilisateur et on lui renvoie la page de connexion */
+                //       delete $sessionStorage.token;
+                //       $state.go('authentification');
+                //       return deffered.reject();
+                //     });
+                //     /*on continue avec lerreur 404 si on atteint ce point*/
+                //     return deffered.promise;
+                //   }
+                //   return rejection;
+                // }
+                /*ici on teste le code alternatif du cas ou on a une mauvaise reponse*/
+                responseError: function(rejection) {
+
+                    // Need to use $injector.get to bring in $state or else we get
+                    // a circular dependency error
+                    var $state = $injector.get('$state');
+
+                    // Instead of checking for a status code of 400 which might be used
+                    // for other reasons in Laravel, we check for the specific rejection
+                    // reasons to tell us if we need to redirect to the login state
+                    var rejectionReasons = ['token_not_provided', 'token_expired', 'token_absent', 'token_invalid'];
+
+                    // Loop through each rejection reason and redirect to the login
+                    // state if one is encountered
+                    angular.forEach(rejectionReasons, function(value, key) {
+
+                        if(rejection.data.error === value) {
+
+                            // If we get a rejection corresponding to one of the reasons
+                            // in our array, we know we need to authenticate the user so
+                            // we can remove the current user from local storage
+                            //localStorage.removeItem('user');
+                            delete $sessionStorage.token;
+                            // Send the user to the auth state so they can login
+                            $ionicLoading.hide();
+                            $state.go('connexion');
+                        }
+                    });
+
+                    return $q.reject(rejection);
+                }
+                /*fin du test de notre code*/
+            };
+
+        }])
+;
+
+
--- www/js/connexion.js
+++ www/js/connexion.js
@@ -1,4 +1,4 @@
-App.controller('ConnexionCtrl', function($scope, $ionicModal, $timeout,$state) {
+App.controller('ConnexionCtrl', function($scope, $ionicModal, $timeout,$state,$auth,$ionicLoading,$sessionStorage) {
 
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
@@ -6,34 +6,32 @@ App.controller('ConnexionCtrl', function($scope, $ionicModal, $timeout,$state) {
     // listen for the $ionicView.enter event:
     //$scope.$on('$ionicView.enter', function(e) {
     //});
-
+$scope.error = "";
     // Form data for the login modal
     $scope.loginData = {};
-
-    // Create the login modal that we will use later
-    /*$ionicModal.fromTemplateUrl('templates/login.html', {
-        scope: $scope
-    }).then(function(modal) {
-        $scope.modal = modal;
-        $scope.modal.show();
-    });*/
-
-    // Triggered in the login modal to close it
-    $scope.closeLogin = function() {
-        $scope.modal.hide();
-    };
-
-    // Open the login modal
-    $scope.login = function() {
-        $scope.modal.show();
-    };
-
+    
     // Perform the login action when the user submits the login form
     $scope.doLogin = function() {
-        console.log('Doing login', $scope.loginData);
+        $ionicLoading.show({
+            templateUrl : 'templates/loading.html'
+        });
+        $auth.login($scope.loginData).then(function (response) {
+            $ionicLoading.hide();
+            if(response.data.success==true){
+                /*on enregistre le token et on passe a la page suivante il faudra egalement verifier
+                * que l'intercepteur ne fonctionne pas normalement*/
+                $sessionStorage.token = response.data.token;
+                $state.go('app.accueil');
+            }else{
+                /*on affiche le message d'erreur*/
+                $scope.error = response.data.error;
+            }
 
+        },function (error) {
+            $ionicLoading.hide();
+        })
         // Simulate a login delay. Remove this and replace with your login
         // code if using a login system
-        $state.go('app.accueil');
+
     };
 })
